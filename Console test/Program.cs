798491c41b2c7a48d800b9cab1562492b5747f71
Kenni Kollemorten <kenn7575@gmail.com>
2023-01-10 13:44:50 +0100

    v2.1

--- Console test/Program.cs
+++ Console test/Program.cs
@@ -14,8 +14,8 @@ namespace Console_test
             while (true)
             {
                 string mode = "";
-                bool valid = false;
-                while (valid == false)
+                bool valid_string = false;
+                while (valid_string == false)
                 {
                     Console.Clear();
 
@@ -44,234 +44,270 @@ namespace Console_test
                         "'IYIL' If Year Is Leap");
                     mode = Console.ReadLine();
                     mode = mode.ToUpper();
-                    
+
                     if (new[] { "CTF", "AAM", "EO", "IRTS", "MO", "TCO", "STN", "AV", "DB", "ICOUL", "INIE", "ISA", "PNOZ", "IYIL" }.Contains(mode))
                     {
-                        valid = true;
+                        valid_string = true;
                     }
                     else
                     {
                         Console.WriteLine("Error: Command didn't mach anything.");
                     }
-                    
+
                 }
 
 
                 ChooseAction(mode);
 
                 Console.ReadKey();
-            }
-            //basic methods
-              void AAM()
-            {
-                Console.Clear();
 
-                Console.Clear();
-                Console.WriteLine("You chose add and multyply \n Type first number: ");
-                string a = Console.ReadLine();
-                Console.WriteLine("Type second number: ");
-                string b = Console.ReadLine();
-                Console.WriteLine("Type third number: ");
-                string c = Console.ReadLine();
-                double ab = double.Parse(a) + double.Parse(b);
-                Console.WriteLine( ab * double.Parse(c));
-            }
-
-              void CTF()
-            {
-                Console.Clear();
-
-                Console.WriteLine("You chose add and multyply \n Type first number: ");
-                string celsius = Console.ReadLine();
-                if (double.Parse(celsius) < -271.15)
+                //basic methods
+                void AAM()
                 {
-                    Console.WriteLine("Error: below absolute zero!");
-                }
-                else
-                {
-                    double placeholder = double.Parse(celsius) * 1.8;
-                    Console.WriteLine(placeholder + 32);
+                    Console.Clear();
+
+                    Console.Clear();
+                    Console.WriteLine("You chose add and multyply \n Type first number: ");
+                    string a = Console.ReadLine();
+                    Console.WriteLine("Type second number: ");
+                    string b = Console.ReadLine();
+                    Console.WriteLine("Type third number: ");
+                    string c = Console.ReadLine();
+                    double ab = double.Parse(a) + double.Parse(b);
+                    Console.WriteLine(ab * double.Parse(c));
                 }
 
-            }
-              void EO()
-            {
-                Console.Clear();
-
-                double a = GetInputAndConvertToDouble("You chose Elementary Operations. \n Type first number: ");
-                double b = GetInputAndConvertToDouble("Type second number: ");
-
-                Console.WriteLine("+:{0} -:{1} *:{2} /:{3}", a + b, a - b, a * b, a / b);
-            }
-            void IRTS()
-            {
-                Console.Clear();
-
-                double a = GetInputAndConvertToDouble("You chose Is Result The Same? \n Type first number: ");
-                double b = GetInputAndConvertToDouble("Type second number: ");
-                if (a + b == a * b)
+                void CTF()
                 {
-                    Console.WriteLine("{2}+{3} ({0}) is the same as {2}*{3} ({1})", a + b, a * b, a, b);
-                }
-                else
-                {
-                    Console.WriteLine("{2}+{3} ({0}) is not the same as {2}*{3} ({1})", a + b, a * b, a, b);
-                }
-                if (a - b == a / b)
-                {
-                    Console.WriteLine("{2}-{3} ({0}) is the same as {2}/{3} ({1})", a - b, a / b, a, b);
-                }
-                else
-                {
-                    Console.WriteLine("{2}-{3} ({0}) is not the same as {2}/{3} ({1})", a - b, a / b, a, b);
-                }
-            }
-              void MO()
-            {
-                Console.Clear();
+                    Console.Clear();
 
-                int a = GetInputAndConvertToInt("You chose Modulo Operations. \n Type first number: ");
-                int b = GetInputAndConvertToInt("Type second number: ");
-                Console.WriteLine("The remainder is {0}", a % b);
-            }
-              void TCO()
-            {
-                Console.Clear();
-                double a = GetInputAndConvertToDouble("You chose The Cupe Of. \n Type a number: ");
-               
-                Console.WriteLine("The remainder is {0}", a * a * a);
-            }
-              void STN()
-            {
-                Console.Clear();
-
-                double a = GetInputAndConvertToDouble("You chose The Cupe Of. \n Type first number: ");
-                double b = GetInputAndConvertToDouble("Type second number: ");
-                Console.WriteLine("Swap: [{0}, {1}]", b, a);
-
-            }
-            //Conditional methods
-
-            void AV() {}
-            void DB() { }
-            void ICOUL() { }
-            void INIE() { }
-            void ISA() { }
-            void PNOZ() { }
-            void IYIL() { }
-
-            
-            //error checking
-            double GetInputAndConvertToDouble(string message)
-            {
-                bool valid = false;
-                double output_value = 0;
-                while (!valid)
-                {
-                    Console.WriteLine(message);
-                    string input_value = Console.ReadLine();
-
-                    bool result = Double.TryParse(input_value, out output_value);
-                    if (result)
+                    Console.WriteLine("You chose add and multyply \n Type first number: ");
+                    string celsius = Console.ReadLine();
+                    if (double.Parse(celsius) < -271.15)
                     {
-                        valid = true;
+                        Console.WriteLine("Error: below absolute zero!");
                     }
                     else
                     {
-                        Console.Clear();
-                        Console.WriteLine("Error: could not convert {0} to int", input_value );
+                        double placeholder = double.Parse(celsius) * 1.8;
+                        Console.WriteLine(placeholder + 32);
                     }
-                }
-                return output_value;
-            }
-            int GetInputAndConvertToInt(string message)
-            {
-                bool valid = false;
-                int output_value = 0;
-                while (!valid)
-                {
-                    Console.WriteLine(message);
-                    string input_value = Console.ReadLine();
 
-                    bool result = Int32.TryParse(input_value, out output_value);
-                    if (result)
+                }
+                void EO()
+                {
+                    Console.Clear();
+
+                    double a = GetInputAndConvertToDouble("You chose Elementary Operations. \n Type first number: ");
+                    double b = GetInputAndConvertToDouble("Type second number: ");
+
+                    Console.WriteLine("+:{0} -:{1} *:{2} /:{3}", a + b, a - b, a * b, a / b);
+                }
+                void IRTS()
+                {
+                    Console.Clear();
+
+                    double a = GetInputAndConvertToDouble("You chose Is Result The Same? \n Type first number: ");
+                    double b = GetInputAndConvertToDouble("Type second number: ");
+                    if (a + b == a * b)
                     {
-                        valid = true;
+                        Console.WriteLine("{2}+{3} ({0}) is the same as {2}*{3} ({1})", a + b, a * b, a, b);
                     }
                     else
                     {
-                        Console.Clear();
-                        Console.WriteLine("Error: could not convert '{0}' to a double", input_value);
+                        Console.WriteLine("{2}+{3} ({0}) is not the same as {2}*{3} ({1})", a + b, a * b, a, b);
+                    }
+                    if (a - b == a / b)
+                    {
+                        Console.WriteLine("{2}-{3} ({0}) is the same as {2}/{3} ({1})", a - b, a / b, a, b);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{2}-{3} ({0}) is not the same as {2}/{3} ({1})", a - b, a / b, a, b);
                     }
                 }
-                return output_value;
-            }
-
-            //Openig menu
-            void ChooseAction(string action)
-            {
-                switch (action)
+                void MO()
                 {
+                    Console.Clear();
 
-                    case "AAM":
-                        AAM();
+                    int a = GetInputAndConvertToInt("You chose Modulo Operations. \n Type first number: ");
+                    int b = GetInputAndConvertToInt("Type second number: ");
+                    Console.WriteLine("The remainder is {0}", a % b);
+                }
+                void TCO()
+                {
+                    Console.Clear();
+                    double a = GetInputAndConvertToDouble("You chose The Cupe Of. \n Type a number: ");
 
-                        break;
-                    case "CTF":
-                        CTF();
+                    Console.WriteLine("The remainder is {0}", a * a * a);
+                }
+                void STN()
+                {
+                    Console.Clear();
 
-                        break;
-                    case "EO":
-                        EO();
+                    double a = GetInputAndConvertToDouble("You chose The Cupe Of. \n Type first number: ");
+                    double b = GetInputAndConvertToDouble("Type second number: ");
+                    Console.WriteLine("Swap: [{0}, {1}]", b, a);
 
-                        break;
-                    case "IRTS":
-                        IRTS();
+                }
+                //Conditional methods
 
-                        break;
-                    case "MO":
-                        MO();
+                void AV()
+                {
+                    double a = GetInputAndConvertToDouble("You chose Absolute value. \n Type a number: ");
+                    Console.WriteLine("The Absolute value of {1} is {0}", Math.Abs(a), a);
+                }
+                void DB()
+                {
+                    double a = GetInputAndConvertToDouble("You chose Divisible by 2 or 3. \n Type first number: ");
+                    double b = GetInputAndConvertToDouble("Type second number: ");
+                    if ((a % 2 == 0 || a % 3 == 0) && (b % 2 == 0 || b % 3 == 0))
+                    {
+                        Console.WriteLine("Both numbers can be devided by 2 or 3. Result: {0}", a * b);
+                    }
+                    else
+                    {
+                        Console.WriteLine("One or both numbers can't be devided by 2 or 3. Result: {0}", a + b);
 
-                        break;
-                    case "TCO":
-                        TCO();
+                    }
+                }
+                void ICOUL()
+                {
+                    bool valid = false;
+                    string str = "";
+                    while (!valid)
+                    {
+                        Console.WriteLine("You chose Divisible by 2 or 3. \n Type a word thats only 3 character long : ");
+                        str = Console.ReadLine();
+                        if (str.Length == 3)
+                        {
+                            valid = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("invalid value. Try again.");
+                        }
+                        bool isUpper = Char.IsUpper(str, 0) && Char.IsUpper(str, 1) && Char.IsUpper(str, 2);
+                        Console.WriteLine("It is {0} that all characters is uppercase", isUpper);
+                    }
+                   
+                }
+                void INIE() { }
+                void ISA() { }
+                void PNOZ() { }
+                void IYIL() { }
+                //error checking
+                double GetInputAndConvertToDouble(string message)
+                {
+                    bool valid = false;
+                    double output_value = 0;
+                    while (!valid)
+                    {
+                        Console.WriteLine(message);
+                        string input_value = Console.ReadLine();
 
-                        break;
-                    case "STN":
-                        STN();
+                        bool result = Double.TryParse(input_value, out output_value);
+                        if (result)
+                        {
+                            valid = true;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Error: could not convert {0} to int", input_value);
+                        }
+                    }
+                    return output_value;
+                }
+                int GetInputAndConvertToInt(string message)
+                {
+                    bool valid = false;
+                    int output_value = 0;
+                    while (!valid)
+                    {
+                        Console.WriteLine(message);
+                        string input_value = Console.ReadLine();
 
-                        break;
-                    case "AV":
-                        AV();
+                        bool result = Int32.TryParse(input_value, out output_value);
+                        if (result)
+                        {
+                            valid = true;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Error: could not convert '{0}' to a double", input_value);
+                        }
+                    }
+                    return output_value;
+                }
 
-                        break;
-                    case "DB":
-                        DB();
+                //Openig menu
+                void ChooseAction(string action)
+                {
+                    switch (action)
+                    {
 
-                        break;
-                    case "ICOUL":
-                        ICOUL();
+                        case "AAM":
+                            AAM();
 
-                        break;
-                    case "INIE":
-                        INIE();
+                            break;
+                        case "CTF":
+                            CTF();
 
-                        break;
-                    case "ISA":
-                        ISA();
+                            break;
+                        case "EO":
+                            EO();
 
-                        break;
-                    case "PNOZ":
-                        PNOZ();
+                            break;
+                        case "IRTS":
+                            IRTS();
 
-                        break;
-                    case "IYIL":
-                        IYIL();
+                            break;
+                        case "MO":
+                            MO();
 
-                        break;
-                    default:
+                            break;
+                        case "TCO":
+                            TCO();
 
-                        break;
+                            break;
+                        case "STN":
+                            STN();
+
+                            break;
+                        case "AV":
+                            AV();
+
+                            break;
+                        case "DB":
+                            DB();
+
+                            break;
+                        case "ICOUL":
+                            ICOUL();
+
+                            break;
+                        case "INIE":
+                            INIE();
+
+                            break;
+                        case "ISA":
+                            ISA();
+
+                            break;
+                        case "PNOZ":
+                            PNOZ();
+
+                            break;
+                        case "IYIL":
+                            IYIL();
+
+                            break;
+                        default:
+
+                            break;
+                    }
                 }
             }
         }
