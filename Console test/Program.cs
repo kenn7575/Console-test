bf28b79d17e3f0a3978a76e6ffa59274e25146b5
Kenni Kollemorten <kenn7575@gmail.com>
2023-01-10 11:01:38 +0100

    Final

--- Console test/Program.cs
+++ Console test/Program.cs
@@ -17,16 +17,24 @@ namespace Console_test
             {
                 Console.Clear();
 
-                Console.WriteLine("What action do you want to perform? \n options: \n 'CTF': Celsius To Fahrenheit. \n 'AAM': Add And Multiply.");
+                Console.WriteLine("What action do you want to perform? \n" +
+                    " options: \n" +
+                    " 'CTF': Celsius To Fahrenheit. \n" +
+                    " 'AAM': Add And Multiply. \n" +
+                    "'EO' Elementary Operations. \n" +
+                    "'IRTS' Is Result The Same? \n" +
+                    "'MO' Modulo Operations. \n" +
+                    "'TCO' The Cupe Of. \n" +
+                    "'STN' Swap Two Numbers.");
             mode = Console.ReadLine();
             mode = mode.ToUpper();
-                if (mode == "CTF" || mode == "AAM")
+                if (mode == "CTF" || mode == "AAM" || mode == "EO" || mode == "IRTS" || mode == "MO" || mode == "TCO" || mode == "STN")
                 {
                     valid = true;
                 }
                 else
                 {
-                    Console.WriteLine("Error: Command didn't mach anything. ");
+                    Console.WriteLine("Error: Command didn't mach anything.");
                 }
             }
             
@@ -66,23 +74,90 @@ namespace Console_test
             }
            
         }
+        private static void EO() {
+            double a = GetInputAndConvertToDouble("You chose Elementary Operations. \n Type first number: ");
+            double b = GetInputAndConvertToDouble("Type second number: ");
+
+            Console.WriteLine("+:{0} -:{1} *:{2} /:{3}", a+b, a-b, a*b, a/b);
+        }
+        private static void IRTS()
+        {
+           
+        }
+        private static void MO()
+        {
+
+        }
+        private static void TCO()
+        {
+
+        }
+        private static void STN()
+        {
+
+        }
+
+        static double GetInputAndConvertToDouble(string message)
+        {
+            bool valid = false;
+            double output_value = 0;
+            while (!valid)
+            {
+                Console.WriteLine(message);
+                string input_value = Console.ReadLine();
+
+                bool result = Double.TryParse(input_value, out output_value);
+                if (result)
+                {
+                    Console.WriteLine("Converted '{0}' to {1}.", input_value, output_value);
+                    valid = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error: could not convert '{0}' to a double", output_value);
+                }
+            }
+            return output_value;
+        }
         private static double ChooseAction(string action)
         {
             switch (action)
             {
                 
                 case "AAM":
-                    return AAM();
+                     AAM();
 
                     break;
                 case "CTF":
-                    return CTF();
+                     CTF();
 
                     break;
+                case "EO":
+                     EO();
+
+                    break;
+                case "IRTS":
+                     IRTS();
+
+                    break;
+                case "MO":
+                     MO();
+
+                    break;
+                case "TCO":
+                     TCO();
+
+                    break;
+                case "STN":
+                     STN();
+
+                    break;
+               
                 default:
                     return 0;
                     break;
             }
         }
+
     }
 }
